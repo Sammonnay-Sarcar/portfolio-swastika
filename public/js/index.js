cd54b90c470b9de898e3656d1731dc98fe66b7ce
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-04-15 17:07:46 +0530

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+let toggleButton = document.getElementById("toggle-button");
+let navbar = document.getElementById("navbar");
+ toggleButton.addEventListener("click", ()=>{
+     navbar.classList.toggle("hidden");
+
+ })
